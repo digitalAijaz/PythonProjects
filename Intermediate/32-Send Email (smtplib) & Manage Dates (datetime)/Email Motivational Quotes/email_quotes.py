4fcc64209fd6a8832ca57ab86a7f967163a346b4
Aijaz Digital <aijaz@iisc.ac.in>
2023-08-31 12:03:50 +0530

    Email Birthday Wishes and Motivational Quotes

---
+++ Intermediate/32-Send Email (smtplib) & Manage Dates (datetime)/Email Motivational Quotes/email_quotes.py
@@ -0,0 +1,31 @@
+import smtplib
+import datetime as dt
+import random
+my_email = "xyz@gmail.com"
+password = "gfgsrereergergerggr"
+
+day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+def send_email():
+    # Create a new SMTP object connection
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        # call TLS (Transport Layer Security) to secure the connection to the email server
+        connection.starttls()
+
+        # login to the email server
+        connection.login(user=my_email, password=password)
+        # send email
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="visionaijaz@gmail.com",
+            msg= f"Subject:{day[day_of_week]} Motivational Quote\n\n{message}"
+        )
+
+now = dt.datetime.now()
+day_of_week = now.weekday()
+if day_of_week ==3:
+    # read quotes.txt file
+    with open("quotes.txt") as file:
+        quotes = file.readlines()
+        message = quotes[random.randint(0, len(quotes) - 1)]
+    send_email()
---
+++ Intermediate/32-Send Email (smtplib) & Manage Dates (datetime)/main.py
@@ -0,0 +1,35 @@
+
+import datetime as dt
+import random
+today = dt.datetime.now()
+month = today.month
+day = today.day
+today_tuple = (month, day)
+
+import pandas
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    import smtplib
+    my_email = "gregf@gmail.com"
+    password = "gwvqgrwthtewthwjjt"
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
+
+
+
+
